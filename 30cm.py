ebbf92ad574d5d236dd77de4a6aa673990105e7f
kant1223 <131114390+kant1223@users.noreply.github.com>
2023-04-26 10:24:24 +0800

    Add files via upload

--- 30cm.py
+++ 30cm.py
@@ -276,9 +276,9 @@ def linebot():
 #     app.run()
 #     port=port
 # import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
     
 
 
